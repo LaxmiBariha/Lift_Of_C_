eb87feca8f16cc86a97791dee57cb29fe43edb3d
LaxmiBariha <78476957+LaxmiBariha@users.noreply.github.com>
2021-02-04 22:54:31 +0530

    Update Q2.c

--- week1/Q2.c
+++ week1/Q2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main() {
-    printf("Enter the temperature in celcius: ");
     float t,fa;
+    printf("Enter the temperature in celcius: ");
     scanf("%f", &t);
     fa = (9*t)/5 + 32;
     printf("So the temperature in Fahrenheit is: %f", fa );
